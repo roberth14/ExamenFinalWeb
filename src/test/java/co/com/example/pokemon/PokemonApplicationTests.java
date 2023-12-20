417c1b6d02b0a61917dfe813d48b74e9927a70cd
roberth14 <roberthdavidcamo@ufps.edu.co>
2023-12-20 08:37:13 -0500

    first commit

---
+++ src/test/java/co/com/example/pokemon/PokemonApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.example.pokemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokemonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
